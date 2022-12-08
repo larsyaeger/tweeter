e0333c805b6f4839e826fc19c36e3799d4ed5c0a
larsyaeger <larsyaeger@gmail.com>
2022-12-07 18:21:58 -0700

    implemented timeago and fixed some bugs and overall made site prettier

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,31 +7,31 @@
 
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 const createTweetElement = function(tweet) {
   const $tweet = $(` <article class="alltweetsarticle">
@@ -54,7 +54,7 @@ const createTweetElement = function(tweet) {
   </div>
   <footer class="allTweetsFooter">
     <div class="tweetAge">
-      <h6>${new Date(tweet.created_at)} </h6>
+       <h6>${timeago.format(tweet.created_at)} </h6>
       </div>
     <div class="likesharesubscribe">
       <i class="fa-solid fa-flag"></i>
@@ -66,16 +66,32 @@ const createTweetElement = function(tweet) {
   return $tweet;
 };
 const renderTweets = function(tweets) {
+  $('#tweets-container').empty();
   for (let tweet of tweets) {
     let $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
+    $('#tweets-container').prepend($tweet);
   }
 };
+const loadTweets = function() {
+  $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+  .then(function (data) {
+    console.log('Success: ', renderTweets(data));
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
 $(document).ready(() => {
-  renderTweets(data);
+  //renderTweets(data);
+  loadTweets();
   $("form").on("submit", function(event) {
     event.preventDefault();
-    console.log($(this).serialize);
-    $.post('/tweets', $(this).serialize());
+   // console.log($(this).serialize);
+    $.post('/tweets', $(this).serialize(), () => {
+      loadTweets();
+    });
+    
   });
 });
+
+//new Date(tweet.created_at)
